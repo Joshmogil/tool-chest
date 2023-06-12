e169d0c732e731c2b0596a33198db71f82ccba93
Josh Mogil <jmogil@pop-os.localdomain>
2023-06-12 15:37:21 -0400

    fixed alias

--- swagger/install.sh
+++ swagger/install.sh
@@ -6,7 +6,7 @@ if grep -Fxq "alias swagger" ~/.bashrc
 then
     echo "swagger alias already set up"
 else
-    echo "alias swagger='docker run --rm -it  --user $(id -u):$(id -g) -e GOPATH=$(go env GOPATH):/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger'" >> ~/.bashrc
+    echo 'alias swagger="docker run --rm -it  --user $(id -u):$(id -g) -e GOPATH=$(go env GOPATH):/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger"' >> ~/.bashrc
 fi
 alias swagger='docker run --rm -it  --user $(id -u):$(id -g) -e GOPATH=$(go env GOPATH):/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger'
 swagger version
